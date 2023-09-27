32d95b05ba155e6b2d61e886ba33e51e97997302
Krutik Aghera <agherakrutik99@gmail.com>
2023-09-27 18:26:10 +0530

    scipt and package

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,3 @@
-const mongoose = require('mongoose')
+import { connect } from 'mongoose'
 
-mongoose.connect('mongodb://127.0.0.1/CampusLink')
+connect('mongodb://127.0.0.1/CampusLink')
--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,31 @@
-const express= require('express')
-const config = require('./db/config')
-const User = require('./db/Users')
-const cors = require('cors')
+import express, { json } from 'express'
+import config from './db/config'
+import User from './db/Users'
+import cors from 'cors'
 
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(json())
 
 
-app.post("/login",async(req,resp)=>{
-     let user = await User.findOne(req.body).select('-password')
-    console.log( req)
-    if(req.body.password && req.body.email){
-     
-     if(user){
-          resp.send(user)
-         }
-         else{
-          resp.send({result : 'User not found'})
-         }
+app.post("/login", async (req, resp) => {
+    let user = await User.findOne(req.body).select('-password')
+    console.log(req)
+    if (req.body.password && req.body.email) {
+
+        if (user) {
+            resp.send(user)
+        }
+        else {
+            resp.send({ result: 'User not found' })
+        }
     }
-    else{
-     resp.send({result : 'User not found'})
+    else {
+        resp.send({ result: 'User not found' })
     }
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('yess i am listening');
 })
